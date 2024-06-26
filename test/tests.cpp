57a1ae466f73fa74f716cd679e46dbb39299ca37
protomem <protomem0709@gmail.com>
2024-06-26 09:29:23 +0300

    chore: add tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,20 +3,35 @@
 #include "neuron.hpp"
 
 using namespace std;
+using namespace neuron;
 
-TEST(Neuron, Neuron)
+TEST(math, dotProduct)
 {
-    neuron::Neuron n;
-    ASSERT_TRUE(1 == 1);
+    ASSERT_EQ(math::dotProduct({ 1.0, 2.0, 3.0 }, { 1.0, 2.0, 3.0 }), 14.0);
+    ASSERT_EQ(math::dotProduct({ 1.0, 2.0, 3.0 }, { 4.0, 5.0, 6.0 }), 32.0);
 }
 
-TEST(Neuron, Neuron2)
+TEST(math, sigmoid)
 {
-    neuron::Neuron n;
-    ASSERT_TRUE(1 == 2);
+    ASSERT_NEAR(math::sigmoid(0.0), 0.5, 0.001);
+    ASSERT_NEAR(math::sigmoid(1.0), 0.7310585786300049, 0.001);
+    ASSERT_NEAR(math::sigmoid(-1.0), 0.2689414213699951, 0.001);
 }
 
-int main(int argc, char **argv)
+TEST(math, derivativeSigmoid)
+{
+    ASSERT_NEAR(math::derivativeSigmoid(0.0), 0.25, 0.001);
+    ASSERT_NEAR(math::derivativeSigmoid(1.0), 0.19661193324148185, 0.001);
+    ASSERT_NEAR(math::derivativeSigmoid(-1.0), 0.19661193324148185, 0.001);
+}
+
+TEST(Neuron, forward)
+{
+    Neuron n({ 0.1, 0.2, 0.3 }, 0.0, math::sigmoid, math::derivativeSigmoid);
+    ASSERT_NEAR(n.forward({ 1.0, 2.0, 3.0 }), 0.80218388855858169, 0.001);
+}
+
+int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
