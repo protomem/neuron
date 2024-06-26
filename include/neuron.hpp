0effd93e7bd13ce7d0786735926274b02c5d3cc9
protomem <protomem0709@gmail.com>
2024-06-26 09:13:33 +0300

    feat: add Neuron

--- include/neuron.hpp
+++ include/neuron.hpp
@@ -1,18 +1,35 @@
 #pragma once
 
+#include <vector>
+
 namespace neuron {
 
+using ActivationFunction = double (*)(double);
+
 class Neuron {
+private:
+    const double learningRate_;
+
+    std::vector<double> weights_;
+
+    double output_;
+    double delta_;
+
+    ActivationFunction activationFn_;
+    ActivationFunction activationDerivativeFn_;
+
 public:
-    Neuron();
-    ~Neuron();
+    Neuron(std::vector<double> weights, double learningRate,
+        ActivationFunction activationFn, ActivationFunction activationDerivativeFn);
+
+    double forward(const std::vector<double>& inputs);
 };
 
 }
 
 namespace neuron::math {
 
-double dotProduct(const std::vector<double> &a, const std::vector<double> &b);
+double dotProduct(const std::vector<double>& xs, const std::vector<double>& ys);
 
 double sigmoid(double x);
 double derivativeSigmoid(double x);
--- src/Neuron.cpp
+++ src/Neuron.cpp
@@ -2,14 +2,21 @@
 
 namespace neuron {
 
-Neuron::Neuron()
+Neuron::Neuron(std::vector<double> weights, double learningRate,
+    ActivationFunction activationFn, ActivationFunction activationDerivativeFn)
+    : learningRate_(learningRate)
+    , weights_(weights)
+    , output_(0.0)
+    , delta_(0.0)
+    , activationFn_(activationFn)
+    , activationDerivativeFn_(activationDerivativeFn)
 {
-    // TODO
 }
 
-Neuron::~Neuron()
+double Neuron::forward(const std::vector<double>& inputs)
 {
-    // TODO
+    output_ = math::dotProduct(weights_, inputs);
+    return activationFn_(output_);
 }
 
 }
